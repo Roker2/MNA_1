49d85f2ca56d111562efc734e33b9e865d249fdf
Roker2 <dimaminko2000@gmail.com>
2020-02-25 21:59:00 +0300

    Add template of work
    Template from http://blog.harrix.org/article/1813 with using CrazyMath Library

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,5 +16,37 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-
+    //Declaring a math function
+    auto x_x = CrazyMath::Pow(CrazyMath::X, 2);
+    double a = -2;//left
+    double b = 2;//right
+    double h = 0.01;//step
+    int N = (b - a) / h + 2;//quantity of steps
+    QVector<double> x(N), y(N);//coordinates
+    int i = 0;
+    for (double X = a; X <= b; X += h) {
+        x[i] = X;
+        y[i] = x_x(X);
+        i++;
+    }
+    //clear
+    ui->widget->clearGraphs();
+    ui->widget->addGraph();
+    //Add to our graph data
+    ui->widget->graph(0)->setData(x, y);
+    //Set labels of Axis
+    ui->widget->xAxis->setLabel("x");
+    ui->widget->yAxis->setLabel("y");
+    //Set range of X
+    ui->widget->xAxis->setRange(a, b);
+    //Set range of Y Axis
+    double minY = y[0], maxY = y[0];
+    for (int i=1; i<N; i++)
+    {
+        if (y[i]<minY) minY = y[i];
+        if (y[i]>maxY) maxY = y[i];
+    }
+    ui->widget->yAxis->setRange(minY, maxY);
+    //Draw graphic
+    ui->widget->replot();
 }
