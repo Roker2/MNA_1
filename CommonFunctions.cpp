7b7d550e518740fe9716fc50a7aeebccc7de159a
Roker2 <dimaminko2000@gmail.com>
2020-02-27 12:40:50 +0300

    CommonFunctions: Add QuadraticEquation function

--- CommonFunctions.cpp
+++ CommonFunctions.cpp
@@ -11,3 +11,20 @@ void MinMaxY(double *minY, double *maxY, QVector<double> y)
             *maxY = y[i];
     }
 }
+
+//ax^2+bx+c=0
+int QuadraticEquation(double a, double b, double c, int *first, int *second)
+{
+    double D = b*b - 4 * c * a;
+    if (D > 0) {
+        *first = (-b + sqrt(D)) / (2 * a);
+        *second = (-b - sqrt(D)) / (2 * a);
+    }
+    else if (D == 0) {
+        *first = (-b) / (2 * a);
+    }
+    else {
+        return -1;//D < 0
+    }
+    return 0;//All done
+}
--- CommonFunctions.h
+++ CommonFunctions.h
@@ -2,7 +2,9 @@
 #define COMMONFUNCTIONS_H
 
 #include <QVector>
+#include <cmath>
 
 void MinMaxY(double *minY, double *maxY, QVector<double> y);
+int QuadraticEquation(double a, double b, double c, int *first, int *second);
 
 #endif // COMMONFUNCTIONS_H
